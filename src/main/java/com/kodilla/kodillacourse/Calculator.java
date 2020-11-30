2f75689b069752a00d18676dfc542520646c7253
Marek Wiech <wieszcz@data.pl>
2020-11-30 03:39:40 +0100

    first commit

--- src/main/java/com/kodilla/kodillacourse/Calculator.java
+++ src/main/java/com/kodilla/kodillacourse/Calculator.java
@@ -6,15 +6,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Calculator {
 
-    public void someMethod() {
+    private int a;
+    private int b;
 
+    public void Calculator() {
+
+    }
+
+    public int dodaj(int a, int b) {
+        this.a = a;
+        this.b = b;
+        return a + b;
+    }
+
+    public int odejmij(int a, int b) {
+        this.a = a;
+        this.b = b;
+        return a - b;
     }
 
     public static void main(String[] args) {
         //SpringApplication.run(KodillaCourseApplication.class, args);
         //test
         Calculator calculator = new Calculator();
-        calculator.someMethod();
+        System.out.println("Dodajemy 3 i 5");
+        System.out.println(calculator.dodaj(3, 5));
+        System.out.println("Odejmujemy 9 i 3");
+        System.out.println(calculator.odejmij(9, 3));
         System.out.println("Zadziałało.");
     }
 
